f238cfdd48fe53bbaf7dd418fc9d3459422783a4
MUEHLHEIM Marc <marc.muehlheim@hexagonsi.com>
2021-04-07 10:32:29 +0200

    Add validation for workSteps #2

---
+++ backend/Backend/Validation/WorkStepValidation.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Model;
+using FluentValidation;
+
+namespace Backend.Validation
+{
+    public class WorkStepValidation : AbstractValidator<WorkStep>
+    {
+        public WorkStepValidation()
+        {
+            RuleFor(x => x.Reporter).NotEmpty();
+            RuleFor(x => x.Description).MaximumLength(200);
+            RuleFor(x => x.State).NotNull();
+        }
+    }
+}
