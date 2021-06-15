10084bef755f2546e9c9f2db71a038afb147126d
Marc Muehlheim <marc.muehlheim@hftm.ch>
2021-06-15 16:36:20 +0200

    Fix authentication

--- app/src/components/AuthenticateUserB2C.js
+++ app/src/components/AuthenticateUserB2C.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import { makeRedirectUri, useAuthRequest, useAutoDiscovery } from 'expo-auth-session';
-import { Button, Platform } from 'react-native';
+import { Button, Text, Platform } from 'react-native';
 
 WebBrowser.maybeCompleteAuthSession();
 
 const AuthenticateUserB2C = ({ login, tenantId, clientId, scope, iosRedirect, androidRedirect }) => {
   // Endpoint
   const discovery = useAutoDiscovery(`https://login.microsoftonline.com/${tenantId}/v2.0`);
+
   // Request
   const [request, response, promptAsync] = useAuthRequest(
     {
